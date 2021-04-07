2a8220f8659b2bbf56b4c8a02e5f3ddc12c7891e
Prince Kumar <princekumar@Princes-MacBook-Air.local>
2021-04-07 22:06:05 +0530

    layout partially done 3

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -4,7 +4,7 @@ import "./Progress.scss";
 function Progress() {
   return (
     <div className="progress">
-      <div className="bar"></div>
+      <div className="bar" style={{ width: "50%" }}></div>
     </div>
   );
 }
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -4,8 +4,20 @@ import "./Question.scss";
 function Question() {
   return (
     <div className="question-container">
-      <div className="question"></div>
-      <div className="option"></div>
+      <div className="question">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero sed
+          placeat explicabo molestiae veniam nihil magni praesentium quisquam
+          enim rem.
+        </p>
+      </div>
+      <hr />
+      <div className="option">
+        <div className="btn">option1</div>
+        <div className="btn">option2</div>
+        <div className="btn">option3</div>
+        <div className="btn">option4</div>
+      </div>
     </div>
   );
 }
